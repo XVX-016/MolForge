b9dbc2962dd035c983197c18f6df9e0a6ec57ac5
XVX-016 <tanmmay2005@gmail.com>
2025-12-20 04:45:07 +0530

    Fix build environment: restore engine package, setup backend, config supabase

---
+++ packages/engine/src/index.ts
@@ -0,0 +1,11 @@
+// Main export file for @biosynth/engine package
+export * from "./types";
+export * from "./MoleculeGraph";
+export * from "./LayoutEngine";
+export * from "./UndoStack";
+export * from "./MoleculeSerializer";
+export * from "./Vec3";
+export * from "./ForceField";
+export * from "./ValenceChecker";
+export * from "./AutoBond";
+
